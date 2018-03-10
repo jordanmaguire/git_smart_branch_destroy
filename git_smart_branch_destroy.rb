8c1a24b2ffa32e27560edb0686ebfb1d48037397
Jordan Maguire <a.jordan.maguire@gmail.com>
2018-03-10 14:31:02 +0800

    Delete a branch, quick

---
+++ git_smart_branch_destroy.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require_relative "lib/branch_parser"
+require_relative "lib/branch_selection_prompt"
+
+branches        = BranchParser.new(`git branch`).branches
+selected_branch = BranchSelectionPrompt.new(branches).prompt_for_branch_selection
+
+puts # Add a line of whitespace to the output for readability
+if selected_branch
+  `git branch -D #{selected_branch}`
+  puts "Destroyed #{selected_branch}"
+else
+  puts "Invalid branch selection"
+end
