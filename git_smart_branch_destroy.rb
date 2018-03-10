680cf6633f4001ab7069f3c265cb0c2611c08044
Jordan Maguire <a.jordan.maguire@gmail.com>
2018-03-10 14:36:27 +0800

    Delete multiple branches!

--- git_smart_branch_destroy.rb
+++ git_smart_branch_destroy.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
 
 require_relative "lib/branch_parser"
-require_relative "lib/branch_selection_prompt"
+require_relative "lib/multiple_branch_selection_prompt"
 
-branches        = BranchParser.new(`git branch`).branches
-selected_branch = BranchSelectionPrompt.new(branches).prompt_for_branch_selection
+branches          = BranchParser.new(`git branch`).branches
+selected_branches = MultipleBranchSelectionPrompt.new(branches).prompt_for_branch_selection
 
 puts # Add a line of whitespace to the output for readability
-if selected_branch
-  `git branch -D #{selected_branch}`
-  puts "Destroyed #{selected_branch}"
+if selected_branches.any?
+  selected_branches.each do |selected_branch|
+    `git branch -D #{selected_branch}`
+    puts "Destroyed #{selected_branch}"
+  end
 else
   puts "Invalid branch selection"
 end
--- lib/multiple_branch_selection_prompt.rb
+++ lib/multiple_branch_selection_prompt.rb
@@ -1,4 +1,4 @@
-class BranchSelectionPrompt
+class MultipleBranchSelectionPrompt
 
   NUMBER_REGEXP = /\A\d+\z/
 
@@ -16,23 +16,26 @@ class BranchSelectionPrompt
 private
 
   def get_branch_name_from_user_input(user_input)
-    if user_input =~ NUMBER_REGEXP
-      chosen_branch_index = user_input.to_i - 1
-      if chosen_branch_index != -1 && !(chosen_branch_name = branches[chosen_branch_index]).nil?
-        chosen_branch_name
+    branch_numbers = user_input.split(" ")
+    branch_numbers.map do |branch_number|
+      if branch_number =~ NUMBER_REGEXP
+        chosen_branch_index = branch_number.to_i - 1
+        if chosen_branch_index != -1 && !(chosen_branch_name = branches[chosen_branch_index]).nil?
+          chosen_branch_name
+        end
       end
-    end
+    end.compact
   end
 
   def print_prompt_for_branch_selection
-    puts "Merge a branch into #{`git symbolic-ref --short HEAD`.strip}:"
+    puts "Destroy one or more branches:"
     puts
     branches.each_with_index do |branch_name, index|
       puts "  #{index+1}: #{branch_name}"
     end
     puts
     # Use print rather than puts so that the number the user types show on the same line
-    print "Type the number of the branch you want to merge: "
+    print "Type the number(s) of the branch(es) you want to destroy: "
   end
 
 end
